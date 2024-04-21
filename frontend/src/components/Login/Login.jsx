40543410bb4ef104060a0e10af6268478e154f38
605s <ploki7335@naver.com>
2024-04-21 17:58:07 +0900

    feat: 로그인 프론트엔드 구현

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const navigate = useNavigate();
+  const server_url = process.env.REACT_APP_SERVER_URL;
+
   const [inputId, setInputId] = useState("");
   const [inputPw, setInputPw] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("id") !== null) {
+      setIsLogin(true);
+    }
+  }, []);
   const handleInputId = (e) => {
     setInputId(e.target.value);
   };
@@ -12,38 +22,68 @@ function Login() {
   const handleInputPw = (e) => {
     setInputPw(e.target.value);
   };
-  const onClickLogin = () => {
-    console.log("click login");
+  const onClickLogin = async () => {
+    if (inputId !== "" && inputPw !== "") {
+      const postUrl = server_url + "/users/login";
+      await axios
+        .post(postUrl, {
+          user_number: inputId,
+          password: inputPw,
+        })
+        .then((res) => {
+          console.log(res.data);
+          localStorage.clear();
+          localStorage.setItem("id", res.data.user_id);
+          window.location.replace("http://localhost:3000/");
+        })
+        .catch((err) => {
+          alert(err);
+        });
+      alert("로그인 성공");
+      navigate("/");
+    } else {
+      alert("모든 내용을 입력해주세요");
+    }
   };
   return (
-    <div className="w-100 h-100 d-flex flex-column justify-items-center align-items-center">
-      <h2 className="mb-4">Login</h2>
-      <div>
-        <label htmlFor="input_id">ID : </label>
-        <input
-          type="text"
-          name="input_id"
-          value={inputId}
-          onChange={handleInputId}
-        />
-      </div>
-      <div>
-        <label htmlFor="input_pw">PW : </label>
-        <input
-          type="password"
-          name="input_pw"
-          value={inputPw}
-          onChange={handleInputPw}
-        />
-      </div>
-      <Button
-        variant="outline-success"
-        className="my-4"
-        onClick={() => onClickLogin()}
-      >
-        로그인
-      </Button>
-    </div>
+    <>
+      {isLogin !== false && (
+        <div className="w-100 h-100 d-flex flex-column justify-items-center align-items-center">
+          <h2 className="mb-4">Login</h2>
+          <div>
+            <div className="mb-3">
+              <label htmlFor="input_id" style={{ width: "40px" }}>
+                ID:
+              </label>
+              <input
+                type="text"
+                name="input_id"
+                value={inputId}
+                onChange={handleInputId}
+              />
+            </div>
+            <div>
+              <label htmlFor="input_pw" style={{ width: "40px" }}>
+                PW:
+              </label>
+              <input
+                type="password"
+                name="input_pw"
+                value={inputPw}
+                onChange={handleInputPw}
+              />
+            </div>
+          </div>
+          <Button
+            variant="outline-success"
+            className="my-4"
+            onClick={() => onClickLogin()}
+          >
+            로그인
+          </Button>
+        </div>
+      )}
+    </>
   );
 }
 
